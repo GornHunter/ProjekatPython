4e623e51707bce216bffc81b6eb083884dcbb181
itke96 <46505588+itke96@users.noreply.github.com>
2019-01-10 00:15:41 +0100

    Uradjen meni
    Dodata slika i dva buttona

--- Movement.py
+++ Movement.py
@@ -1,24 +1,44 @@
 import sys
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap, QPainter, QBrush
-from PyQt5.QtWidgets import QWidget, QLabel, QApplication
-
+from PyQt5.QtCore import Qt, QRect, QTimer, pyqtSignal
+from PyQt5.QtGui import QPixmap, QIcon, QPainter
+from PyQt5.QtWidgets import QLabel, QApplication, QMainWindow, QPushButton, QDesktopWidget
 
 from key_notifier import KeyNotifier
 
 
-class SimMoveDemo(QWidget):
+class Movement(QMainWindow):
 
-    def __init__(self):
-       super().__init__()
+    closed = pyqtSignal()
 
-        self.pix1 = QPixmap('airplane2.png')
-        self.pix2 = QPixmap('airplane3.png')
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        self.parent = parent
+
+        self.moveSpeed = 13
+        self.bulletCounter1 = 0
+        self.bulletCounter2 = 0
+        self.bulletList1 = []
+        self.bulletList2 = []
+        self.seaPos = 0
+
+        self.timer = QTimer(self)
+        self.timer.setSingleShot(False)
+        self.timer.setInterval(10)
+        self.timer.timeout.connect(self.frame)
+        self.timer.start(10)
+
+        self.setGeometry(300, 30, 1024, 768)
+        self.setMinimumSize(1024, 768)
+        self.setMaximumSize(1024, 768)
+
+        self.pix1 = QPixmap('player1.png')
+        self.pix2 = QPixmap('player2.png')
         self.label1 = QLabel(self)
         self.label2 = QLabel(self)
 
-        self.setWindowState(Qt.WindowMaximized)
+        self.seaTex = QPixmap("sea.png").scaled(70, 77)
+
         self.__init_ui__()
 
         self.key_notifier = KeyNotifier()
@@ -26,13 +46,16 @@ class SimMoveDemo(QWidget):
         self.key_notifier.start()
 
     def __init_ui__(self):
+
         self.label1.setPixmap(self.pix1)
-        self.label1.setGeometry(1800, 900, 50, 50)
+        self.label1.setGeometry(848, 674, 120, 95)
 
         self.label2.setPixmap(self.pix2)
-        self.label2.setGeometry(50, 900, 50, 50)
+        self.label2.setGeometry(63, 674, 112, 96)
 
         self.setWindowTitle('1942')
+        self.setWindowIcon(QIcon('icon.png'))
+        self.show()
 
     def keyPressEvent(self, event):
         self.key_notifier.add_key(event.key())
@@ -41,40 +64,138 @@ class SimMoveDemo(QWidget):
         self.key_notifier.rem_key(event.key())
 
     def __update_position__(self, key):
-        rec1 = self.label1.geometry()
-        rec2 = self.label2.geometry()
+        rec1: QRect = self.label1.geometry()
+        rec2: QRect = self.label2.geometry()
 
         if key == Qt.Key_Right:
-            self.label1.setGeometry(rec1.x() + 13, rec1.y(), rec1.width(), rec1.height())
+            if rec1.x() + rec1.width() <= 1024:
+                self.label1.setGeometry(rec1.x() + self.moveSpeed, rec1.y(), rec1.width(), rec1.height())
         elif key == Qt.Key_Down:
-            self.label1.setGeometry(rec1.x(), rec1.y() + 13, rec1.width(), rec1.height())
+            if rec1.y() + rec1.height() <= 768:
+                self.label1.setGeometry(rec1.x(), rec1.y() + self.moveSpeed, rec1.width(), rec1.height())
         elif key == Qt.Key_Up:
-            self.label1.setGeometry(rec1.x(), rec1.y() - 13, rec1.width(), rec1.height())
+            if rec1.y() > 1:
+                self.label1.setGeometry(rec1.x(), rec1.y() - self.moveSpeed, rec1.width(), rec1.height())
         elif key == Qt.Key_Left:
-            self.label1.setGeometry(rec1.x() - 13, rec1.y(), rec1.width(), rec1.height())
+            if rec1.x() > 0:
+                self.label1.setGeometry(rec1.x() - self.moveSpeed, rec1.y(), rec1.width(), rec1.height())
+        elif key == Qt.Key_M:
+            if self.bulletCounter1 == 0:
+                self.bulletCounter1 = 20
+                temp = QLabel(self)
+                temp.setGeometry(rec1.x() + rec1.width() / 2 - 4, rec1.y() - 10, 10, 10)
+                temp.setPixmap(QPixmap('bullet1.png'))
+                temp.show()
+                self.bulletList1.append(temp)
 
         if key == Qt.Key_D:
-            self.label2.setGeometry(rec2.x() + 13, rec2.y(), rec2.width(), rec2.height())
+            if rec2.x() + rec2.width() <= 1024:
+                self.label2.setGeometry(rec2.x() + self.moveSpeed, rec2.y(), rec2.width(), rec2.height())
         elif key == Qt.Key_S:
-            self.label2.setGeometry(rec2.x(), rec2.y() + 13, rec2.width(), rec2.height())
+            if rec2.y() + rec2.height() <= 768:
+                self.label2.setGeometry(rec2.x(), rec2.y() + self.moveSpeed, rec2.width(), rec2.height())
         elif key == Qt.Key_W:
-            self.label2.setGeometry(rec2.x(), rec2.y() - 13, rec2.width(), rec2.height())
+            if rec2.y() > 1:
+                self.label2.setGeometry(rec2.x(), rec2.y() - self.moveSpeed, rec2.width(), rec2.height())
         elif key == Qt.Key_A:
-            self.label2.setGeometry(rec2.x() - 13, rec2.y(), rec2.width(), rec2.height())
+            if rec2.x() > 0:
+                self.label2.setGeometry(rec2.x() - self.moveSpeed, rec2.y(), rec2.width(), rec2.height())
+        elif key == Qt.Key_R:
+            if self.bulletCounter2 == 0:
+                self.bulletCounter2 = 20
+                temp = QLabel(self)
+                temp.setGeometry(rec2.x() + rec2.width() / 2 - 4, rec2.y() - 10, 10, 10)
+                temp.setPixmap(QPixmap('bullet2.png'))
+                temp.show()
+                self.bulletList2.append(temp)
 
         if key == Qt.Key_Escape:
             self.close()
 
     def closeEvent(self, event):
         self.key_notifier.die()
+        self.closed.emit()
+        event.accept()
 
-    def paintEvent(self, event):
-         p = QPainter(self)
+    def paintEvent(self, *args, **kwargs):
 
-         p.fillRect(event.rect(), QBrush(Qt.black))
+        p = QPainter(self)
 
+        for i in range(0, 15):
+            for j in range(-1, 10):
+                p.drawPixmap(i * self.seaTex.width(), j * self.seaTex.height() + self.seaPos, self.seaTex)
+
+    def frame(self):
+        if self.bulletCounter1 > 0:
+            self.bulletCounter1 -= 1
+
+        if self.bulletCounter2 > 0:
+            self.bulletCounter2 -= 1
+
+        self.seaPos += 1
+        if self.seaPos > self.seaTex.height():
+            self.seaPos = 0
+
+        for bullet1 in self.bulletList1:
+            rec: QRect = bullet1.geometry()
+            bullet1.setGeometry(rec.x(), rec.y() - 3, rec.width(), rec.height())
+            if rec.y() < -10:
+                bullet1.clear()
+                self.bulletList1.remove(bullet1)
+            # if rec.intersects(self.label2.geometry()):
+            #    bullet.clear()
+            #    self.bulletList.remove(bullet)
+        self.update()
+
+        for bullet2 in self.bulletList2:
+            rec: QRect = bullet2.geometry()
+            bullet2.setGeometry(rec.x(), rec.y() - 3, rec.width(), rec.height())
+            if rec.y() < -10:
+                bullet2.clear()
+                self.bulletList2.remove(bullet2)
+            # if rec.intersects(self.label1.geometry()):
+            #    bullet.clear()
+            #    self.bulletList.remove(bullet)
+        self.update()
+
+class Menu(QMainWindow):
+
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+
+        self.logo = QPixmap('logo.png')
+        self.label = QLabel(self)
+        self.label.setPixmap(self.logo)
+        self.label.setGeometry(0, 0, 800, 720)
+
+        self.setGeometry(300, 30, 800, 720)
+        self.resize(self.logo.width(), self.logo.height())
+        self.setMinimumSize(self.logo.width(), self.logo.height())
+        self.setMaximumSize(self.logo.width(), self.logo.height())
+        self.setWindowTitle('1942')
+        self.setWindowIcon(QIcon('icon.png'))
+
+        buttonPlay = QPushButton('START GAME', self)
+        buttonPlay.clicked.connect(self.gameWindow)
+        buttonPlay.resize(320, 40)
+        buttonPlay.move(40, 320)
+
+        buttonExit = QPushButton('EXIT', self)
+        buttonExit.clicked.connect(self.exit)
+        buttonExit.resize(320, 40)
+        buttonExit.move(440, 320)
+
+    def gameWindow(self):
+        self.game = Movement(self)
+        self.game.closed.connect(self.show)
+        self.game.show()
+        self.hide()
+
+    def exit(self):
+        self.close()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = SimMoveDemo()
+    ex = Menu()
+    ex.show()
     sys.exit(app.exec_())
