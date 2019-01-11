ceee604bd8a7b4c24e07345171ddf2d930a537ea
SaraJ96 <j.sara1996@gmail.com>
2019-01-11 14:01:27 +0100

    popravljanje bagova

--- Movement.py
+++ Movement.py
@@ -1,27 +1,30 @@
 import sys
-import turtle
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap, QIcon, QPainter, QPaintEvent, QBrush
-from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QAction, QMainWindow
+from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QHBoxLayout, QAction, QMainWindow
 
 from key_notifier import KeyNotifier
 
 
-class SimMoveDemo(QMainWindow):
+class Movement(QMainWindow):
 
     def __init__(self):
         super().__init__()
+
+        #self.setWindowState(Qt.WindowMaximized)
+        self.setGeometry(600, 300, 800, 600)
+        hbox = QHBoxLayout(self)
+        pixmap = QPixmap("sea.jpg")
+        lbl = QLabel(self)
+        lbl.setPixmap(pixmap)
+        hbox.addWidget(lbl)
+        self.setLayout(hbox)
 
         self.pix1 = QPixmap('player1.png')
         self.pix2 = QPixmap('player2.png')
         self.label1 = QLabel(self)
         self.label2 = QLabel(self)
-
-        #self.setWindowState(Qt.WindowMaximized)
-        self.setGeometry(600, 300, 800, 600)
-
-        self.setStyleSheet("QWidget {background-image: url(sea.jpg)}")
 
         self.__init_ui__()
 
@@ -31,14 +34,25 @@ class SimMoveDemo(QMainWindow):
 
     def __init_ui__(self):
 
+        exitAct = QAction(QIcon('skull.png'), 'Exit 1942', self)
+        exitAct.setShortcut('Ctrl+Q')
+        #exitAct.setStatusTip('Exit')
+        exitAct.triggered.connect(self.close)
+
+        #self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&Game')
+        fileMenu.addAction(exitAct)
+
         self.label1.setPixmap(self.pix1)
-        self.label1.setGeometry(100, 40, 50, 50)
+        self.label1.setGeometry(100, 40, 120, 95)
 
         self.label2.setPixmap(self.pix2)
-        self.label2.setGeometry(50, 40, 50, 50)
+        self.label2.setGeometry(50, 40, 112, 96)
 
         self.setWindowTitle('1942')
-
+        self.setWindowIcon(QIcon('icon.png'))
         self.show()
 
     def keyPressEvent(self, event):
@@ -52,27 +66,28 @@ class SimMoveDemo(QMainWindow):
         rec2 = self.label2.geometry()
 
         if key == Qt.Key_Right:
-            self.label1.setGeometry(rec1.x() + 13, rec1.y(), rec1.width(), rec1.height())
+            self.label1.setGeometry(rec1.x() + 5, rec1.y(), rec1.width(), rec1.height())
         elif key == Qt.Key_Down:
-            self.label1.setGeometry(rec1.x(), rec1.y() + 13, rec1.width(), rec1.height())
+            self.label1.setGeometry(rec1.x(), rec1.y() + 5, rec1.width(), rec1.height())
         elif key == Qt.Key_Up:
-            self.label1.setGeometry(rec1.x(), rec1.y() - 13, rec1.width(), rec1.height())
+            self.label1.setGeometry(rec1.x(), rec1.y() - 5, rec1.width(), rec1.height())
         elif key == Qt.Key_Left:
-            self.label1.setGeometry(rec1.x() - 13, rec1.y(), rec1.width(), rec1.height())
+            self.label1.setGeometry(rec1.x() - 5, rec1.y(), rec1.width(), rec1.height())
 
         if key == Qt.Key_D:
-            self.label2.setGeometry(rec2.x() + 13, rec2.y(), rec2.width(), rec2.height())
+            self.label2.setGeometry(rec2.x() + 5, rec2.y(), rec2.width(), rec2.height())
         elif key == Qt.Key_S:
-            self.label2.setGeometry(rec2.x(), rec2.y() + 13, rec2.width(), rec2.height())
+            self.label2.setGeometry(rec2.x(), rec2.y() + 5, rec2.width(), rec2.height())
         elif key == Qt.Key_W:
-            self.label2.setGeometry(rec2.x(), rec2.y() - 13, rec2.width(), rec2.height())
+            self.label2.setGeometry(rec2.x(), rec2.y() - 5, rec2.width(), rec2.height())
         elif key == Qt.Key_A:
-            self.label2.setGeometry(rec2.x() - 13, rec2.y(), rec2.width(), rec2.height())
+            self.label2.setGeometry(rec2.x() - 5, rec2.y(), rec2.width(), rec2.height())
 
     def closeEvent(self, event):
         self.key_notifier.die()
 
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = SimMoveDemo()
+    ex = Movement()
     sys.exit(app.exec_())
