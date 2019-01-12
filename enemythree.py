4c3247031d39cf6a33b8dd73f9edff5bd9e7cfd4
GornHunter <igigotika@gmail.com>
2019-01-12 10:04:57 +0100

    dodata lista

--- enemythree.py
+++ enemythree.py
@@ -5,8 +5,8 @@ class EnemyThree(Enemy):
     def __init__(self, mainWindow):
         super().__init__(mainWindow)
 
-        self.health = 30
-        self.width = 225
-        self.height = 125
+        self.health = 3
+        self.width = 300
+        self.height = 166
         self.speed = 2
         self.label.setPixmap(mainWindow.enemy3)
--- enemytwo.py
+++ enemytwo.py
@@ -5,8 +5,8 @@ class EnemyTwo(Enemy):
     def __init__(self, mainWindow):
         super().__init__(mainWindow)
 
-        self.health = 20
-        self.width = 75
-        self.height = 61
+        self.health = 2
+        self.width = 123
+        self.height = 119
         self.speed = 2
         self.label.setPixmap(mainWindow.enemy2)
