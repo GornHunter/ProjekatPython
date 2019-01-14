60d64fd75ba705baae68116b64a7ec2ed5b8a109
GornHunter <igigotika@gmail.com>
2019-01-14 11:00:19 +0100

    napravljena Deus Ex Machina

--- movement.py
+++ movement.py
@@ -7,6 +7,10 @@ from enemyone import EnemyOne
 from enemytwo import EnemyTwo
 from enemythree import EnemyThree
 
+import threading
+import time
+import random
+
 
 class Movement(QMainWindow):
     closed = pyqtSignal()
@@ -27,6 +31,9 @@ class Movement(QMainWindow):
         self.lives_left_player2 = 3
         self.player1_score = 0
         self.player2_score = 0
+        self.planeCounter = 1
+
+        self.interval = 2
 
         self.timer = QTimer(self)
         self.timer.setSingleShot(False)
@@ -37,6 +44,9 @@ class Movement(QMainWindow):
         self.setGeometry(450, 70, 1024, 900)
         self.setMinimumSize(1024, 900)
         self.setMaximumSize(1024, 900)
+
+        self.randomForce = QPixmap('images/bullet1.png').scaled(15, 25)
+        self.DeusExMachina = QLabel(self)
 
         self.life3_player1 = QPixmap('images/life3_player1.png')
         self.life2_player1 = QPixmap('images/life2_player1.png')
@@ -67,8 +77,6 @@ class Movement(QMainWindow):
 
         self.seaTex = QPixmap("images/sea.png").scaled(70, 77)
 
-        self.planeCounter = 1
-
         self.__init_ui__()
 
         self.key_notifier = KeyNotifier()
@@ -83,7 +91,7 @@ class Movement(QMainWindow):
         self.label2.setGeometry(55, 760, 75, 57)
 
         self.label_player1.setPixmap(self.life3_player1)
-        self.label_player1.setGeometry(850, 835, 150, 33) # 30, 835, 150, 33
+        self.label_player1.setGeometry(850, 835, 150, 33)
 
         self.label_player2.setPixmap(self.life3_player2)
         self.label_player2.setGeometry(30, 835, 150, 33)
@@ -94,9 +102,51 @@ class Movement(QMainWindow):
         self.score_player2.setGeometry(24, 3, 200, 100)
         self.score_player2.setText(" 2UP\n {0}".format(self.player2_score))
 
+        self.DeusExMachina.setPixmap(self.randomForce)
+        self.DeusExMachina.setGeometry(random.randint(124, 900), random.randint(450, 874), 12, 25)
+
+        self.thread = threading.Thread(target=self.justDoIt, args=())
+        self.thread.daemon = True
+        self.thread.start()
+
         self.setWindowTitle('1942')
         self.setWindowIcon(QIcon('images/icon.png'))
         self.show()
+
+    def justDoIt(self):
+        while True:
+            player1: QRect = self.label1.geometry()
+            player1.setCoords(player1.x() + 30, player1.y(), player1.x() + 45, player1.y() + 60)
+            player2: QRect = self.label2.geometry()
+            player2.setCoords(player2.x() + 30, player2.y(), player2.x() + 45, player2.y() + 60)
+            randomForce: QRect = self.DeusExMachina.geometry()
+
+            # time.sleep(self.interval)
+            # self.DeusExMachina.setVisible(1)
+            if player1.intersects(randomForce):
+                if self.DeusExMachina.isVisible() == True:
+                    self.DeusExMachina.hide()
+                    self.DeusExMachina.setVisible(0)
+                    time.sleep(self.interval)
+                    self.player1_score += 100
+                    self.score_player1.setText(" 1UP\n {0}".format(self.player1_score))
+                else:
+                    pass
+
+            if player2.intersects(randomForce):
+                if self.DeusExMachina.isVisible() == True:
+                    self.DeusExMachina.hide()
+                    self.DeusExMachina.setVisible(0)
+                    time.sleep(self.interval)
+                    self.player2_score += 100
+                    self.score_player2.setText(" 2UP\n {0}".format(self.player2_score))
+                else:
+                    pass
+
+            if self.DeusExMachina.isVisible() == False:
+                time.sleep(0.1)
+                self.DeusExMachina.setVisible(1)
+                self.DeusExMachina.setGeometry(random.randint(124, 900), random.randint(450, 874), 12, 25)
 
     def keyPressEvent(self, event):
         self.key_notifier.add_key(event.key())
@@ -110,75 +160,75 @@ class Movement(QMainWindow):
 
         if key == Qt.Key_Right:
             if rec1.x() + rec1.width() <= 1024:
-                if self.label1.isVisible() == 0:
+                if self.label1.isVisible() == False:
                     pass
                 else:
                     self.label1.setGeometry(rec1.x() + self.moveSpeed, rec1.y(), rec1.width(), rec1.height())
         elif key == Qt.Key_Down:
             if rec1.y() + rec1.height() <= 900:
-                if self.label1.isVisible() == 0:
+                if self.label1.isVisible() == False:
                     pass
                 else:
                     self.label1.setGeometry(rec1.x(), rec1.y() + self.moveSpeed, rec1.width(), rec1.height())
         elif key == Qt.Key_Up:
             if rec1.y() > 1:
-                if self.label1.isVisible() == 0:
+                if self.label1.isVisible() == False:
                     pass
                 else:
                     self.label1.setGeometry(rec1.x(), rec1.y() - self.moveSpeed, rec1.width(), rec1.height())
         elif key == Qt.Key_Left:
             if rec1.x() > 0:
-                if self.label1.isVisible() == 0:
+                if self.label1.isVisible() == False:
                     pass
                 else:
                     self.label1.setGeometry(rec1.x() - self.moveSpeed, rec1.y(), rec1.width(), rec1.height())
         elif key == Qt.Key_L:
-            if self.label1.isVisible() == 0:
+            if self.label1.isVisible() == False:
                 pass
             else:
                 if self.bulletCounter1 == 0:
                     self.bulletCounter1 = 20
                     temp = QLabel(self)
                     temp.setObjectName(" 1")
-                    temp.setGeometry(rec1.x() + rec1.width() / 2 - 4, rec1.y() - 10, 10, 15)
-                    temp.setPixmap(QPixmap('images/bullet1.png').scaled(10, 15))
+                    temp.setGeometry(rec1.x() + rec1.width() / 2 - 4, rec1.y() - 10, 7, 12)
+                    temp.setPixmap(QPixmap('images/bullet1.png').scaled(7, 12))
                     temp.show()
                     self.bulletListP.append(temp)
 
         if key == Qt.Key_D:
             if rec2.x() + rec2.width() <= 1024:
-                if self.label2.isVisible() == 0:
+                if self.label2.isVisible() == False:
                     pass
                 else:
                     self.label2.setGeometry(rec2.x() + self.moveSpeed, rec2.y(), rec2.width(), rec2.height())
         elif key == Qt.Key_S:
             if rec2.y() + rec2.height() <= 900:
-                if self.label2.isVisible() == 0:
+                if self.label2.isVisible() == False:
                     pass
                 else:
                     self.label2.setGeometry(rec2.x(), rec2.y() + self.moveSpeed, rec2.width(), rec2.height())
         elif key == Qt.Key_W:
             if rec2.y() > 1:
-                if self.label2.isVisible() == 0:
+                if self.label2.isVisible() == False:
                     pass
                 else:
                     self.label2.setGeometry(rec2.x(), rec2.y() - self.moveSpeed, rec2.width(), rec2.height())
         elif key == Qt.Key_A:
             if rec2.x() > 0:
-                if self.label2.isVisible() == 0:
+                if self.label2.isVisible() == False:
                     pass
                 else:
                     self.label2.setGeometry(rec2.x() - self.moveSpeed, rec2.y(), rec2.width(), rec2.height())
         elif key == Qt.Key_R:
-            if self.label2.isVisible() == 0:
+            if self.label2.isVisible() == False:
                 pass
             else:
                 if self.bulletCounter2 == 0:
                     self.bulletCounter2 = 20
                     temp = QLabel(self)
                     temp.setObjectName(" 2")
-                    temp.setGeometry(rec2.x() + rec2.width() / 2 - 4, rec2.y() - 10, 10, 15)
-                    temp.setPixmap(QPixmap('images/bullet1.png').scaled(10, 15))
+                    temp.setGeometry(rec2.x() + rec2.width() / 2 - 4, rec2.y() - 10, 7, 12)
+                    temp.setPixmap(QPixmap('images/bullet1.png').scaled(7, 12))
                     temp.show()
                     self.bulletListP.append(temp)
 
@@ -223,19 +273,10 @@ class Movement(QMainWindow):
                     self.enemies[i].setPosition(i % 6 * 150 + 100, -100)
                     self.enemies[i].moveTo(100 + i % 6 * 150, 100 + i // 6 * 100)
 
-                # self.enemies[i].setPosition((5-i) * 150 - 100, -100)
-                # self.enemies[i].moveTo(100 + i % 6 * 150, 100 + i // 6 * 100)
-
             self.enemyCounter += 1
             if self.enemyCounter == 3:
                 self.enemyCounter = 0
                 self.planeCounter += 1
-                '''if len(self.enemies) == 0:
-                    if len(self.enemies2) == 0:
-                        for j in range(0, 5):
-                            self.enemies2.append(EnemyTwo(self))
-                            self.enemies2[j].setPosition(j * (-150) - 100, -100)
-                            self.enemies2[j].moveTo(160 + j * 150, 100)'''
 
         for enemy1 in self.enemies:
             enemy1.update()
@@ -277,22 +318,9 @@ class Movement(QMainWindow):
 
                     break
 
-            '''for enemy2 in self.enemies2:
-                if rec.intersects(enemy2.label.geometry()):
-                    bullet.clear()
-                    self.bulletListP.remove(bullet)
-                    if enemy2.hit():
-                        enemy2.label.clear()
-                        self.enemies2.remove(enemy2)
-                        if bullet.objectName() == " 1":
-                            self.player1_score += 20
-                            self.score_player1.setText(" 1UP\n {0}".format(self.player1_score))
-                        else:
-                            self.player2_score += 20
-                            self.score_player2.setText(" 2UP\n {0}".format(self.player2_score))'''
-
         for bullet in self.bulletListE:
             rec: QRect = bullet.geometry()
+            randomForce: QRect = self.DeusExMachina.geometry()
             player1: QRect = self.label1.geometry()
             player1.setCoords(player1.x() + 30, player1.y(), player1.x() + 45, player1.y() + 60)
             player2: QRect = self.label2.geometry()
@@ -301,6 +329,24 @@ class Movement(QMainWindow):
             if rec.y() > 910:
                 bullet.clear()
                 self.bulletListE.remove(bullet)
+
+            '''if player1.intersects(randomForce):
+                if self.DeusExMachina.isVisible() == True:
+                    self.player1_score += 100
+                    self.score_player1.setText(" 1UP\n {0}".format(self.player1_score))
+                    self.DeusExMachina.hide()
+                    self.DeusExMachina.setVisible(0)
+                else:
+                    pass
+
+            if player2.intersects(randomForce):
+                if self.DeusExMachina.isVisible() == True:
+                    self.player2_score += 100
+                    self.score_player2.setText(" 2UP\n {0}".format(self.player2_score))
+                    self.DeusExMachina.hide()
+                    self.DeusExMachina.setVisible(0)
+                else:
+                    pass'''
 
             if self.label1.isVisible() == True:
                 if rec.intersects(player1):
